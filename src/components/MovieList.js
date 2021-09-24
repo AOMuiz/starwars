b6a7f038fb6565fc5a7c86dd468d38451f6498d6
AOMuiz <abdulwasiumuiz@gmail.com>
2021-09-24 09:08:05 +0100

    created globalStyle with Styled Components

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import useMovieList from "../hooks/useMovieList";
 import Loader from "../shared/Loader";
 import MovieDetail from "./MovieDetail";
